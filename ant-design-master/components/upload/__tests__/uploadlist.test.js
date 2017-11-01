d5468d8a099f3961005525e941bf9f7572364854
xietian <xietian@jk.cn>
2017-11-01 21:47:05 +0800

    antd

---
+++ ant-design-master/components/upload/__tests__/uploadlist.test.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Upload from '..';
+import { errorRequest, successRequest } from './requests';
+
+const delay = timeout => new Promise(resolve => setTimeout(resolve, timeout));
+
+const fileList = [{
+  uid: -1,
+  name: 'xxx.png',
+  status: 'done',
+  url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  thumbUrl: 'https://zos.alipayobjects.com/rmsportal/IQKRngzUuFzJzGzRJXUs.png',
+}, {
+  uid: -2,
+  name: 'yyy.png',
+  status: 'done',
+  url: 'https://zos.alipayobjects.com/rmsportal/IQKRngzUuFzJzGzRJXUs.png',
+  thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+}];
+
+describe('Upload List', () => {
+  // https://github.com/ant-design/ant-design/issues/4653
+  it('should use file.thumbUrl for <img /> in priority', () => {
+    const wrapper = mount(
+      <Upload defaultFileList={fileList} listType="picture">
+        <button>upload</button>
+      </Upload>
+    );
+    fileList.forEach((file, i) => {
+      const linkNode = wrapper.find('.ant-upload-list-item-thumbnail').at(i);
+      const imgNode = wrapper.find('.ant-upload-list-item-thumbnail img').at(i);
+      expect(linkNode.prop('href')).toBe(file.url);
+      expect(imgNode.prop('src')).toBe(file.thumbUrl);
+    });
+  });
+
+  // https://github.com/ant-design/ant-design/issues/7269
+  it('should remove correct item when uid is 0', async () => {
+    const list = [{
+      uid: 0,
+      name: 'xxx.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+      thumbUrl: 'https://zos.alipayobjects.com/rmsportal/IQKRngzUuFzJzGzRJXUs.png',
+    }, {
+      uid: 1,
+      name: 'xxx.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+      thumbUrl: 'https://zos.alipayobjects.com/rmsportal/IQKRngzUuFzJzGzRJXUs.png',
+    }];
+    const wrapper = mount(
+      <Upload defaultFileList={list}>
+        <button>upload</button>
+      </Upload>
+    );
+    expect(wrapper.find('.ant-upload-list-item').length).toBe(2);
+    wrapper.find('.ant-upload-list-item').at(0).find('.anticon-cross').simulate('click');
+    await delay(400);
+    expect(wrapper.find('.ant-upload-list-item').length).toBe(1);
+  });
+
+  it('should be uploading when upload a file', (done) => {
+    let wrapper;
+    const onChange = ({ file }) => {
+      if (file.status === 'uploading') {
+        expect(wrapper.render()).toMatchSnapshot();
+      }
+      if (file.status === 'done') {
+        expect(wrapper.render()).toMatchSnapshot();
+        done();
+      }
+    };
+    wrapper = mount(
+      <Upload
+        action="http://jsonplaceholder.typicode.com/posts/"
+        onChange={onChange}
+        customRequest={successRequest}
+      >
+        <button>upload</button>
+      </Upload>
+    );
+    wrapper.find('input').simulate('change', {
+      target: {
+        files: [
+          { filename: 'foo.png' },
+        ],
+      },
+    });
+  });
+
+  it('handle error', (done) => {
+    let wrapper;
+    const onChange = ({ file }) => {
+      if (file.status !== 'uploading') {
+        expect(wrapper.render()).toMatchSnapshot();
+        done();
+      }
+    };
+    wrapper = mount(
+      <Upload
+        action="http://jsonplaceholder.typicode.com/posts/"
+        onChange={onChange}
+        customRequest={errorRequest}
+      >
+        <button>upload</button>
+      </Upload>
+    );
+    wrapper.find('input').simulate('change', {
+      target: {
+        files: [
+          { filename: 'foo.png' },
+        ],
+      },
+    });
+  });
+});
