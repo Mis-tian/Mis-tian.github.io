ca10f151cc545fc4120260460ab3fb538b900c98
xietian <xietian@jk.cn>
2017-11-01 22:43:14 +0800

    al

--- docs/resource/github.md.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([91,220],{1292:function(t,e){t.exports={content:["article"],meta:{order:2,link:"//github.com/ant-design/ant-design",title:"GitHub",filename:"docs/resource/github.md"},toc:["ul"]}}});
--- docs/spec/motion.md.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([61,220],{1322:function(t,n){t.exports={content:["article"],meta:{order:12,title:{"zh-CN":"\u52a8\u6548","en-US":"Motion"},link:"http://motion.ant.design",filename:"docs/spec/motion.md"},toc:["ul"]}}});
